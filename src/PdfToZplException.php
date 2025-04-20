20fd0cfe57a85f368bdcf9a3f72d019f4d38db40
Ben Faerber <benfaerber@protonmail.com>
2025-04-20 04:54:29 -0600

    Add doc comment

--- src/PdfToZplException.php
+++ src/PdfToZplException.php
@@ -5,9 +5,7 @@ namespace Faerber\PdfToZpl;
 use Exception;
 use Throwable;
 
-/**
- * Define a custom exception class
- */
+/** A custom exception to let you know this is a library related error */
 class PdfToZplException extends Exception {
     public function __construct(string $message, int $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);

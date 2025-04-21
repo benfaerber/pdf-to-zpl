99dc0f678b1a0d92c9666ddc53f567a7f5f9ee9f
Ben Faerber <benfaerber@protonmail.com>
2025-04-21 08:14:40 -0600

    Some 8.4 stuff

--- src/PdfToZplException.php
+++ src/PdfToZplException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 /** A custom exception to let you know this is a library related error */
 class PdfToZplException extends Exception {
-    public function __construct(string $message, int $code = 0, Throwable $previous = null) {
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 

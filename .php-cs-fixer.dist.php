e6d24b18c96f8ff55bba8a38c227d3399b5242ba
Ben Faerber <benfaerber@protonmail.com>
2025-04-26 01:42:23 -0600

    Make Formatter Parallel

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,7 @@
 <?php
 
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
 $finder = PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . "/src",
@@ -9,10 +11,8 @@ $finder = PhpCsFixer\Finder::create()
     ]);
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
         '@PSR12' => false,
-        'braces' => [
-            'position_after_functions_and_oop_constructs' => 'same',
-        ],
     ])
     ->setFinder($finder);

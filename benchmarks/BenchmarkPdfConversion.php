41ea120b3c7ea8881b9e403569c6f249c2e9f069
Ben Faerber <benfaerber@protonmail.com>
2025-05-02 20:22:28 -0600

    Switch to attributes

--- benchmarks/BenchmarkPdfConversion.php
+++ benchmarks/BenchmarkPdfConversion.php
@@ -1,13 +1,14 @@
 <?php
 
 use Faerber\PdfToZpl\Images\ImageProcessorOption;
+use Faerber\PdfToZpl\Logger\EchoLogger;
 use Faerber\PdfToZpl\PdfToZplConverter;
 use Faerber\PdfToZpl\Settings\ConverterSettings;
 
-/**
- * @Revs(2)
- * @Iterations(2)
- */
+use PhpBench\Attributes\{Revs, Iterations, Groups, Subject};
+
+#[Revs(2)]
+#[Iterations(2)]
 class BenchmarkPdfConversion {
     public static function testFile(string $name): string {
         return __DIR__ . "/../test_data/{$name}";
@@ -24,63 +25,50 @@ class BenchmarkPdfConversion {
     private function convertFileWithProcessor(string $name, ImageProcessorOption $imageProcessor): void {
         $this->convertFile($name, new ConverterSettings(
             imageProcessorOption: $imageProcessor,
+            logger: new EchoLogger,
         ));
     }
 
     /**
      * Verify phpbench is working properly
-     * @Subject
      */
+    #[Subject]
     public function doAdd(): void {
         $b = 1 + 2;
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertLabelImagick(): void {
         $this->convertFileWithProcessor("endicia-shipping-label.pdf", ImageProcessorOption::Imagick);
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertLabelGd(): void {
         $this->convertFileWithProcessor("endicia-shipping-label.pdf", ImageProcessorOption::Gd);
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertDonkeyImagick(): void {
         $this->convertFileWithProcessor("donkey.pdf", ImageProcessorOption::Imagick);
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertDonkeyGd(): void {
         $this->convertFileWithProcessor("donkey.pdf", ImageProcessorOption::Gd);
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertAmericaImagick(): void {
         $this->convertFileWithProcessor("america.pdf", ImageProcessorOption::Imagick);
     }
 
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertAmericaGd(): void {
         $this->convertFileWithProcessor("america.pdf", ImageProcessorOption::Gd);
     }
 
-    /**
-     * @Subject
-     */
+    #[Subject]
     public function doConvertTinyLabel(): void {
         $this->convertFile("endicia-shipping-label.pdf", new ConverterSettings(
             labelWidth: 150,
